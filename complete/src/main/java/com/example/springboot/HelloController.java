55e95070389d824e280cd93c1f291e558b67f92c
Trask Stalnaker <trask.stalnaker@gmail.com>
2022-11-02 16:10:18 -0700

    Add trackMetric

--- complete/src/main/java/com/example/springboot/HelloController.java
+++ complete/src/main/java/com/example/springboot/HelloController.java
@@ -1,13 +1,20 @@
 package com.example.springboot;
 
+import com.microsoft.applicationinsights.TelemetryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicInteger;
 
 @RestController
 public class HelloController {
 
+	private static final TelemetryClient telemetryClient = new TelemetryClient();
+	private static final AtomicInteger counter = new AtomicInteger();
+
 	@GetMapping("/")
 	public String index() {
+		telemetryClient.trackMetric("mymetric", counter.incrementAndGet());
 		return "Greetings from Spring Boot!";
 	}
 
